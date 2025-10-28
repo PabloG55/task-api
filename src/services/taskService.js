07791311ab4c005a87f8cbe0ea185ca8969cc91c
PabloG55 <pasebarona@gmail.com>
2025-10-28 16:40:08 -0400

    Added GET /tasks/:id

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -7,3 +7,13 @@ export async function getAllTasks() {
 export async function createTask(newTask) {
   return taskRepository.create(newTask);
 }
+
+export async function getTaskById(taskId) {
+  let result = await taskRepository.findById(taskId);
+  if (result) return result;
+  else {
+    const error = new Error('Task not found');
+    error.status = 404;
+    throw error;
+  }
+}
